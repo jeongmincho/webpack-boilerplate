5dfec25d356dfc3b9cfab26f2e9a3b06b065e8fa
jeongmincho <jeongcho96@gmail.com>
2021-09-24 22:41:10 -0400

    add separate dev and prod env

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import './styles.css'
+import nft from './nft.png'
+import bunny from './bunny.svg'
+
 export const App = () => {
-    return <h1>React Typescript Webpack Starter Template</h1>
+    return <>
+        <h1>React Typescript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
+        <img src={nft} alt="nft img" width="300" height="300" />
+        <img src={bunny} alt="bunny img" width="300" height="300" />
+    </>
 }
