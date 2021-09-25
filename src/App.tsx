ad61bf668437549dcee739176b6ba2b0d75f5977
jeongmincho <jeongcho96@gmail.com>
2021-09-24 22:58:22 -0400

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './styles.css'
 import nft from './nft.png'
 import bunny from './bunny.svg'
+import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
     return <>
         <h1>React Typescript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
         <img src={nft} alt="nft img" width="300" height="300" />
         <img src={bunny} alt="bunny img" width="300" height="300" />
+        <ClickCounter />
     </>
 }
---
+++ src/ClickCounter.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+export const ClickCounter = () => {
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+            <button onClick={() => setCount(count => count + 1)}>Count {count}</button>
+        </div>
+    )
+}
